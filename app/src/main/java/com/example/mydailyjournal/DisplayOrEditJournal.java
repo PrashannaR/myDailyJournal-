873873d0e707c5be28843d8fb452d9ec7d0b7a52
PrashannaR <prashannna.rajbhandari1@gmail.com>
2022-02-07 12:35:22 +0545

    -

---
+++ app/src/main/java/com/example/mydailyjournal/DisplayOrEditJournal.java
@@ -0,0 +1,14 @@
+package com.example.mydailyjournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayOrEditJournal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_or_edit_journal);
+    }
+}
